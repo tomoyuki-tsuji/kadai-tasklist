69299e1869b832cbcc9eef91bc842e9898344530
tomoyuki.tsuji <tsujit975@gmail.com>
2019-06-28 13:32:15 +0000

    kadai-tasklist

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+                
+                @foreach ($tasks as $tasks)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $tasks->id, ['id' => $tasks->id]) !!}</td>
+                    <td>{{ $tasks->content }}</td>
+                </tr>
+                 
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
+    
+    
+
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>id = {{ $tasks->id }} のタスク詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $tasks->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $tasks->content }}</td>
+        </tr>
+    </table>
+    
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $tasks->id], ['class' => 'btn btn-light']) !!}
+    
+    {!! Form::model($tasks, ['route' => ['tasks.destroy', $tasks->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    
+    {!! Form::close() !!}
+
+@endsection
